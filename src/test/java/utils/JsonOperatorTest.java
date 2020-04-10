2732d4c54b4642ff10b359b782455ebdb81a8423
Maxx Schneiderhahn <mkschneiderh@bsu.edu>
2020-04-10 12:18:55 -0400

    Added moderately working functions, struggled writing tests.

---
+++ src/test/java/utils/JsonOperatorTest.java
@@ -0,0 +1,15 @@
+package utils;
+
+import org.junit.jupiter.api.BeforeEach;
+
+class JsonOperatorTest {
+
+    HTTPUtils httpUtils;
+
+    @BeforeEach
+    void setup() {
+        httpUtils = new HTTPUtils();
+    }
+
+
+}
